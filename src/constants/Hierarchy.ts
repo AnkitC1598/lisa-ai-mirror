1cfe7bf50b820bbdad8ff813b2d19a76ccc878fe
AnkitC1598 <ankitc@letsupgrade.in>
2024-04-08 14:52:26 +0530

    style: hierarchy breadcrumb peek colors updated

--- src/constants/Hierarchy.ts
+++ src/constants/Hierarchy.ts
@@ -9,24 +9,24 @@ const HierarchyConstants: IHierarchyConstant = {
 		icon: TermIcon,
 		colors: {
 			card: {
-				border: "hover:border-pink-400 dark:hover:border-pink-800",
-				icon: "text-pink-300 dark:text-pink-600",
-				bg: "bg-pink-100 dark:bg-pink-800",
-				text: "text-pink-700 dark:text-pink-50",
+				border: "hover:border-violet-400 dark:hover:border-violet-800",
+				icon: "text-violet-300 dark:text-violet-600",
+				bg: "bg-violet-100 dark:bg-violet-800",
+				text: "text-violet-700 dark:text-violet-50",
 			},
 			content: {
-				createIcon: "text-pink-500",
-				dataIcon: "text-pink-200 dark:text-pink-500",
+				createIcon: "text-violet-500",
+				dataIcon: "text-violet-200 dark:text-violet-500",
 			},
 			form: {
-				bg: "bg-pink-800",
-				text: "text-pink-50",
-				lightText: "text-pink-200",
+				bg: "bg-violet-800",
+				text: "text-violet-50",
+				lightText: "text-violet-200",
 			},
-			badge: "bg-pink-50 ring-pink-700/10 dark:bg-pink-400/10 dark:ring-pink-400/30 text-pink-800 dark:text-pink-400",
+			badge: "bg-violet-50 ring-violet-700/10 dark:bg-violet-400/10 dark:ring-violet-400/30 text-violet-800 dark:text-violet-400",
 			peek: {
-				border: "stroke-pink-300 dark:stroke-pink-600",
-				bg: "fill-pink-100 dark:fill-pink-950",
+				border: "stroke-violet-300 dark:stroke-violet-700",
+				bg: "fill-violet-100 dark:fill-violet-950",
 			},
 		},
 	},
@@ -50,7 +50,7 @@ const HierarchyConstants: IHierarchyConstant = {
 			},
 			badge: "bg-pink-50 ring-pink-700/10 dark:bg-pink-400/10 dark:ring-pink-400/30 text-pink-800 dark:text-pink-400",
 			peek: {
-				border: "stroke-pink-300 dark:stroke-pink-600",
+				border: "stroke-pink-300 dark:stroke-pink-700",
 				bg: "fill-pink-100 dark:fill-pink-950",
 			},
 		},
@@ -75,7 +75,7 @@ const HierarchyConstants: IHierarchyConstant = {
 			},
 			badge: "bg-purple-50 ring-purple-700/10 dark:bg-purple-400/10 dark:ring-purple-400/30 text-purple-800 dark:text-purple-400",
 			peek: {
-				border: "stroke-purple-300 dark:stroke-purple-600",
+				border: "stroke-purple-300 dark:stroke-purple-700",
 				bg: "fill-purple-100 dark:fill-purple-950",
 			},
 		},
@@ -100,7 +100,7 @@ const HierarchyConstants: IHierarchyConstant = {
 			},
 			badge: "bg-blue-50 ring-blue-700/10 dark:bg-blue-400/10 dark:ring-blue-400/30 text-blue-800 dark:text-blue-400",
 			peek: {
-				border: "stroke-blue-300 dark:stroke-blue-600",
+				border: "stroke-blue-300 dark:stroke-blue-700",
 				bg: "fill-blue-100 dark:fill-blue-950",
 			},
 		},
@@ -125,7 +125,7 @@ const HierarchyConstants: IHierarchyConstant = {
 			},
 			badge: "bg-rose-50 ring-rose-700/10 dark:bg-rose-400/10 dark:ring-rose-400/30 text-rose-800 dark:text-rose-400",
 			peek: {
-				border: "stroke-rose-300 dark:stroke-rose-600",
+				border: "stroke-rose-300 dark:stroke-rose-700",
 				bg: "fill-rose-100 dark:fill-rose-950",
 			},
 		},
@@ -150,7 +150,7 @@ const HierarchyConstants: IHierarchyConstant = {
 			},
 			badge: "bg-neutral-50 ring-neutral-700/10 dark:bg-neutral-400/10 dark:ring-neutral-400/30 text-neutral-800 dark:text-neutral-400",
 			peek: {
-				border: "stroke-neutral-300 dark:stroke-neutral-600",
+				border: "stroke-neutral-300 dark:stroke-neutral-700",
 				bg: "fill-neutral-100 dark:fill-neutral-950",
 			},
 		},
