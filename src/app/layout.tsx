c37c496bf14fde93d48bb375042424a20a9b2a6c
AnkitC1598 <ankitc@letsupgrade.in>
2024-04-16 11:00:55 +0530

    remove: posthog integration removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import "@/app/globals.css"
 import Loading from "@/components/atoms/Loading"
-import PostHogPageView from "@/components/organisms/PostHog/PageView"
+// import PostHogPageView from "@/components/organisms/PostHog/PageView"
 import { cn } from "@/lib/utils"
 import AuthProvider from "@/providers/authProvider"
 import NextThemeProvider from "@/providers/nextThemeProvider"
-import PostHogProvider from "@/providers/postHogProvider"
+// import PostHogProvider from "@/providers/postHogProvider"
 import type { Viewport } from "next"
 import { Metadata } from "next"
 import { Inter } from "next/font/google"
@@ -37,37 +37,37 @@ const AppLayout: React.FC<Readonly<IAppLayout>> = ({ children }) => {
 			lang="en"
 			suppressHydrationWarning
 		>
-			<PostHogProvider>
-				<body
-					className={cn(
-						inter.className,
-						"flex h-screen w-screen justify-center overflow-hidden bg-neutral-50 text-gray-900 transition-all duration-300 ease-in-out dark:bg-neutral-950 dark:text-gray-200"
-					)}
-				>
-					<main className="flex h-screen w-screen justify-center md:p-4">
-						<NextThemeProvider
-							attribute="class"
-							defaultTheme="dark"
-							enableSystem
-						>
-							<Suspense>
+			{/* <PostHogProvider> */}
+			<body
+				className={cn(
+					inter.className,
+					"flex h-screen w-screen justify-center overflow-hidden bg-neutral-50 text-gray-900 transition-all duration-300 ease-in-out dark:bg-neutral-950 dark:text-gray-200"
+				)}
+			>
+				<main className="flex h-screen w-screen justify-center md:p-4">
+					<NextThemeProvider
+						attribute="class"
+						defaultTheme="dark"
+						enableSystem
+					>
+						{/* <Suspense>
 								<PostHogPageView />
+							</Suspense> */}
+						<div className="relative flex w-full max-w-md flex-col overflow-hidden bg-[url('/lisa-ai/white-gridlines.png')] bg-cover dark:bg-[url('/lisa-ai/starry-sky.png')] md:rounded-md md:border md:border-neutral-500">
+							<Suspense
+								fallback={
+									<div className="flex h-full w-full items-center justify-center py-8">
+										<Loading icon />
+									</div>
+								}
+							>
+								<AuthProvider>{children}</AuthProvider>
 							</Suspense>
-							<div className="relative flex w-full max-w-md flex-col overflow-hidden bg-[url('/lisa-ai/white-gridlines.png')] bg-cover dark:bg-[url('/lisa-ai/starry-sky.png')] md:rounded-md md:border md:border-neutral-500">
-								<Suspense
-									fallback={
-										<div className="flex h-full w-full items-center justify-center py-8">
-											<Loading icon />
-										</div>
-									}
-								>
-									<AuthProvider>{children}</AuthProvider>
-								</Suspense>
-							</div>
-						</NextThemeProvider>
-					</main>
-				</body>
-			</PostHogProvider>
+						</div>
+					</NextThemeProvider>
+				</main>
+			</body>
+			{/* </PostHogProvider> */}
 		</html>
 	)
 }
